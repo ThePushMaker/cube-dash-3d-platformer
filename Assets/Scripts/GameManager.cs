f7df7a76ee5a1175a3d39b58e3952b1534e449b0
Martín Calderón <elmcwl@gmail.com>
2024-08-06 19:32:54 -0700

    add score counter and play button functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting; // TextMesh Pro
 
 public class GameManager : MonoBehaviour
 {
@@ -8,16 +10,20 @@ public class GameManager : MonoBehaviour
     public Transform spawnPoint;
     int score = 0;
 
+    public TextMeshProUGUI scoreText;
+    public GameObject playButton;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        GameStart();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     
     IEnumerator SpawnObstacles()
@@ -31,9 +37,19 @@ public class GameManager : MonoBehaviour
             Instantiate(obstacle, spawnPoint.position, Quaternion.identity);
         }
     }
+    
+    void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
 
     public void GameStart()
     {
+        player.SetActive(true);
+        playButton.SetActive(false);
         StartCoroutine("SpawnObstacles");
+
+        InvokeRepeating("ScoreUp", 2f, 1f);
     }
 }
